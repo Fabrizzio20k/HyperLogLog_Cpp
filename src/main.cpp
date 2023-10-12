bc8ea444a72a4da57be2193058eb0bcaad40d64d
Fabrizzio <fabrizzio.vilchez.e@tutec.edu.pe>
2023-10-12 11:09:43 -0500

    Add extra files

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,7 @@
 
 int main()
 {
-    HyperLogLog hll(4);
-
-    for (int i = 0; i < 1000000; ++i) {
-        hll.insert(to_string(i));
-    }
-
-    cout << "Estimacion de elementos: " << hll.count() << endl;
+    HyperLogLog hll(15);
+    hll.count_from_csv("../mock/esas_mehsullar.csv", "satish_kodu");
+    cout << hll.count() << endl;
 }

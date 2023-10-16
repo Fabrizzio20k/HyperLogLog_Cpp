f18cfb5ec010e127d84feb17fa368b27d23c1791
Fabrizzio <fabrizzio785@gmail.com>
2023-10-16 12:35:42 -0500

    Add extra routes

--- include/api.h
+++ include/api.h
@@ -43,6 +43,132 @@ Api::Api() {
 
         return crow::response(response);
     });
+
+    CROW_ROUTE(app, "/api/v1/hyperloglog/insert").methods("POST"_method)
+    ([this](const crow::request& req) {
+        auto json = crow::json::load(req.body);
+        if (!json)
+            return crow::response(400);
+
+        if (hll == nullptr)
+            return crow::response(400);
+
+        string value = json["value"].s();
+
+        hll->insert(value);
+
+        crow::json::wvalue response;
+
+        response["status"] = "ok";
+        response["message"] = "Value inserted";
+
+        auto x = hll->get_info_structure();
+
+        for(const auto& i:x){
+            response["info"][i.first] = i.second;
+        }
+
+        return crow::response(response);
+    });
+
+    CROW_ROUTE(app, "/api/v1/hyperloglog/info").methods("GET"_method)
+    ([this](const crow::request& req) {
+        if (hll == nullptr)
+            return crow::response(400);
+
+        crow::json::wvalue response;
+
+        response["status"] = "ok";
+        response["message"] = "Info";
+
+        auto x = hll->get_info_structure();
+
+        for(const auto& i:x){
+            response["info"][i.first] = i.second;
+        }
+
+        return crow::response(response);
+    });
+
+    CROW_ROUTE(app, "/api/v1/hyperloglog/reset").methods("POST"_method)
+    ([this](const crow::request& req){
+        if (hll == nullptr)
+            return crow::response(400);
+
+        hll->clear();
+
+        crow::json::wvalue response;
+
+        response["status"] = "ok";
+        response["message"] = "HyperLogLog reseted";
+
+        auto x = hll->get_info_structure();
+
+        for(const auto& i:x){
+            response["info"][i.first] = i.second;
+        }
+
+        return crow::response(response);
+    });
+
+    CROW_ROUTE(app, "/api/v1/hyperloglog/count").methods("GET"_method)
+    ([this](const crow::request& req) {
+        if (hll == nullptr)
+            return crow::response(400);
+
+        crow::json::wvalue response;
+
+        response["status"] = "ok";
+        response["message"] = "Count";
+
+        auto x = hll->count();
+
+        response["count"] = x["count"];
+        response["time"] = x["time"];
+
+        auto y = hll->get_info_structure();
+
+        for(const auto& i:y){
+            response["info"][i.first] = i.second;
+        }
+
+        return crow::response(response);
+    });
+
+    CROW_ROUTE(app,"/api/v1/hyperloglog/csv").methods("POST"_method)
+    ([this](const crow::request& req) {
+        auto json = crow::json::load(req.body);
+        if (!json)
+            return crow::response(400);
+
+        if (hll == nullptr)
+            return crow::response(400);
+
+        string nombreArchivo = json["nombreArchivo"].s();
+        string nombreColumna = json["nombreColumna"].s();
+
+        hll->count_from_csv(nombreArchivo, nombreColumna);
+
+        crow::json::wvalue response;
+
+        response["status"] = "ok";
+        response["message"] = "Count from csv";
+
+        auto x = hll->count();
+
+        response["count"] = x["count"];
+        response["time"] = x["time"];
+
+        auto y = hll->get_info_structure();
+
+        for(const auto& i:y){
+            response["info"][i.first] = i.second;
+        }
+
+        return crow::response(response);
+    });
+
+
 }
 
 void Api::run() {

8c71aab1717a6281f39a0edf7bca6e20bdf19853
Fabrizzio20k <115495332+Fabrizzio20k@users.noreply.github.com>
2023-10-15 22:23:39 -0500

    Update execution of main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
-#include "hyperloglog.h"
+#include "api.h"
 
-using namespace std;
-
-int main(){
-    HyperLogLog hll(16);
-    for (int i = 0; i < 100000000; ++i) {
-        hll.insert(to_string(i));
-    }
-
-    cout << hll.count() << endl;
+int main() {
+    Api api;
+    api.run();
+    return 0;
 }
-
